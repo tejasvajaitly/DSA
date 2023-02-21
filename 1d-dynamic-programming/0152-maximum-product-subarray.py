56b236c0c2b374a2101a1922ec7f8a6d1e9d1d9a
Tejasva Jaitly <tejasvajaitly@gmail.com>
2023-02-20 18:43:19 -0800

    added explanation

--- 1d-dynamic-programming/0152-maximum-product-subarray.py
+++ 1d-dynamic-programming/0152-maximum-product-subarray.py
@@ -10,3 +10,8 @@ class Solution:
             currMax = max(tmp * n, currMax * n, n)
             res = max(res, currMax)
         return res
+
+
+# When working with a dynamic programming approach for this problem, it's important to keep track of both the current maximum and the current minimum product, as multiplying a negative number by the current minimum can potentially result in a new maximum product.
+
+
